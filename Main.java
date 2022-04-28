4bbb9c87fa98b36dbe82fa20391ce935c95c62f5
Yova12 <97071733+Yova12@users.noreply.github.com>
2022-04-28 14:14:42 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Person Person1 = new Person();
+        Person1.setAge(3);
+        Person1.setName("Yovanny");
+        Person1.setPhoneNumber(4983756);
+
+        System.out.println("edad: " + Person1.getAge());
+        System.out.println("nombre: " + Person1.getName());
+        System.out.println("telefono: " + Person1.getPhoneNumber());
+
+    }
+    
+
+}
---
+++ Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    private int age;
+    private String name;
+    private int phoneNumber;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
